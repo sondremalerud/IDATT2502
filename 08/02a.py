8596dd31ee375768f0b3f6a5074250ed4b3d630b
Sondre Malerud <sondre_mal@hotmail.no>
2023-10-19 16:13:03 +0200

    yea

--- 08/02a.py
+++ 08/02a.py
@@ -9,13 +9,16 @@ class GridWorld:
         self.goal_pos = goal_pos
         self.agent_pos = (0, 0)
         self.actions = [(-1, 0), (1, 0), (0, -1), (0, 1)]  # Up, Down, Left, Right
+        self.danger_spaces = [(2, 1), (0, 3), (3, 3), (4, 3)]
         self.state = (0, 0)
         self.done = False
+        self.agent_visited = [self.state]
 
     def reset(self):
         self.agent_pos = (0, 0)
         self.state = (0, 0)
         self.done = False
+        self.agent_visited = [self.state]
         return self.state
 
     def step(self, action):
@@ -28,16 +31,26 @@ class GridWorld:
         if 0 <= new_pos[0] < self.width and 0 <= new_pos[1] < self.height:
             self.agent_pos = new_pos
         else:
-            reward = -1 # crashed into a wall
+            reward = -5 # crashed into a wall
             return self.agent_pos, reward, self.done
 
 
         # Check if the agent reached the goal
         if self.agent_pos == self.goal_pos:
             self.done = True
-            reward = 1
-        else:
+            reward = 10
+        elif self.agent_pos not in self.agent_visited:
+            # new position
+            self.agent_visited.append(self.agent_pos)
             reward = 0
+        elif self.agent_pos in self.danger_spaces:
+            reward = -999
+            self.done = True
+        else:
+            # not a new position
+            reward = -1
+
+        
 
         return self.agent_pos, reward, self.done
 
@@ -64,11 +77,14 @@ class QLearningAgent:
         self.q_table[state[0], state[1], action] = new_q_value
 
 def draw_q_values(q_table):
+    
     for i in range(width):
         for j in range(height):
             actions = q_table[i, j]
             best_action = np.argmax(actions)
-            color = 255 - int(actions[best_action] * 255)
+            color = abs(255 - int(actions[best_action]) * 255) % 255
+            print(int(actions[best_action]) * 255)
+            print(color)
             pygame.draw.rect(screen, (color, color, color), (i * cell_size, j * cell_size, cell_size, cell_size))
 
             # Adjust the angle based on the best action
@@ -97,6 +113,30 @@ def draw_agent(current_position):
 
 
 
+def draw_danger_spaces(danger_spaces):
+    image = pygame.image.load("./08/alien.png")
+    img_size = 30
+    image  = pygame.transform.scale(image, (img_size,img_size))
+    for i in range(width):
+        for j in range(height):
+            # Draw a dot for the current position
+            if (i, j) in danger_spaces:
+
+                 # Draw the image
+                screen.blit(image, (i * cell_size + (cell_size-img_size) // 2, j * cell_size + (cell_size-img_size) // 2))
+
+def draw_goal(goal_pos):
+    image = pygame.image.load("./08/star.png")
+    img_size = 30
+    image  = pygame.transform.scale(image, (img_size,img_size))
+    x = goal_pos[0]
+    y = goal_pos[1]
+    screen.blit(image, (x * cell_size + (cell_size-img_size) // 2, y * cell_size + (cell_size-img_size) // 2))
+
+
+
+
+
 # Main loop
 width, height = 5, 5
 goal_pos = (width - 1, height - 1)
@@ -107,6 +147,12 @@ agent = QLearningAgent(action_space_size=4)
 cell_size = 50
 pygame.init()
 screen = pygame.display.set_mode((width * cell_size, height * cell_size))
+
+screen.fill((255, 255, 255))
+draw_q_values(agent.q_table)
+draw_danger_spaces(env.danger_spaces)
+draw_goal(env.goal_pos)
+pygame.display.flip()
 
 episode = 0
 
@@ -125,7 +171,8 @@ while running:
         agent.update_q_table(state, action, reward, next_state)
 
         state = next_state
-        total_reward += reward
+        total_reward += reward        
+
         draw_agent(env.agent_pos)
         pygame.display.flip()
 
@@ -134,8 +181,10 @@ while running:
     if episode % 100 == 0:
         print(f"Episode: {episode}, Total Reward: {total_reward}")
 
-    screen.fill((255, 255, 255))
+    screen.fill((255,255,255))
     draw_q_values(agent.q_table)
+    draw_danger_spaces(env.danger_spaces)
+    draw_goal(env.goal_pos)
     pygame.display.flip()
 
     episode += 1
